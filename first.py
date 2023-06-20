c1cb262fee6db738d3b5be8ff76b5d93d097e252
ad1mohan <ad1mohan.official@gmail.com>
2023-06-20 11:41:27 +0530

    add changes to make a text file and save answers

--- first.py
+++ first.py
@@ -16,7 +16,7 @@ action= ActionChains(driver)
 #
 file=open("info.csv","w")
 write_o=csv.writer(file, delimiter=',')
-for i in range(100000):
+for i in [97934,97935,90837]:
     try:
         x="https://www.examtopics.com/discussions/amazon/view/"+str(i)+"-exam-aws-certified-solutions-architect-professional-sap-c02/"
         driver.get("https://www.examtopics.com/discussions/amazon/view/"+str(i)+"-exam-aws-certified-solutions-architect-professional-sap-c02/")
@@ -25,9 +25,15 @@ for i in range(100000):
 
         if "Exam AWS Certified Solutions Architect - Professional SAP-C02 topic".upper() in topic:
             questionNumber=topic.split()[-2]
-            print("true")
-            print(questionNumber)
+            print("True",questionNumber)
             write_o.writerow([x,questionNumber])
+            temp = driver.find_element(By.XPATH,"/html/body/div[2]/div/div[3]/div/div[1]/div[2]/div[2]/ul")
+            
+            
+            
+            list = temp.find_element(By.XPATH,".//li[2]")
+            print(list.text)
+
         else:
             print("false")
     except NoSuchElementException:
