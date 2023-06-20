9652caf661167260b8c4f4c12905c9b4686de8eb
Anurag-Anand0308 <anuragananda99@gmail.com>
2023-06-20 15:39:46 +0530

    improvement done

--- first.py
+++ first.py
@@ -1,3 +1,4 @@
+#importing all the required modules
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
@@ -5,17 +6,28 @@ from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 import time
 import csv
-time.time()
+#
+print(time.time())
 
 # creating an option variable and making it headless
+
 opt=Options()
 opt.add_argument('--headless')
-#
+
+#creating driver and action instance
+
 driver=webdriver.Chrome(options=opt)
 action= ActionChains(driver)
-#
+
+#opening a csv file
+
 file=open("info.csv","w")
 write_o=csv.writer(file, delimiter=',')
+
+#opening a txt file
+question_file=open("details.txt","w")
+
+#starting a loop with alll the main code
 for i in [97934,97935,90837]:
     try:
         x="https://www.examtopics.com/discussions/amazon/view/"+str(i)+"-exam-aws-certified-solutions-architect-professional-sap-c02/"
@@ -27,13 +39,27 @@ for i in [97934,97935,90837]:
             questionNumber=topic.split()[-2]
             print("True",questionNumber)
             write_o.writerow([x,questionNumber])
+            question_file.write("QUESTION N0. ")
+            question_file.write(questionNumber+"\n")
+            question=driver.find_element(By.XPATH,"/html/body/div[2]/div/div[3]/div/div[1]/div[2]/p")
+            question_file.write("\n")
+            question_file.write(question.text+"\n")
+            question_file.write("\n")
             temp = driver.find_element(By.XPATH,"/html/body/div[2]/div/div[3]/div/div[1]/div[2]/div[2]/ul")
+            for j in range(1,7):
+                try:
+                    list = temp.find_element(By.XPATH,".//li["+str(j)+"]")
+                    question_file.write(list.text+"\n")
+                except:
+                    pass
+            element2=driver.find_element(By.XPATH,"/html/body/div[2]/div/div[3]/div/div[1]/div[2]/a[1]")
+            action.click(element2)
+            action.perform()
+            answer=driver.find_element(By.XPATH,"/html/body/div[2]/div/div[3]/div/div[1]/div[2]/div[3]/span[1]/span")
+            question_file.write("Answer is  " )
+            question_file.write(answer.text+"\n")
+            question_file.write("\n")
             
-            
-            
-            list = temp.find_element(By.XPATH,".//li[2]")
-            print(list.text)
-
         else:
             print("false")
     except NoSuchElementException:
